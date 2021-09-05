23775f8e3406ce91c8b33c46899fde282d532c32
ika nuraini <ikanuraini778@gmail.com>
2021-09-05 18:02:10 +0700

    IKA

--- application/controllers/Data_hasil.php
+++ application/controllers/Data_hasil.php
@@ -4,25 +4,99 @@ class Data_hasil extends CI_Controller {
     public function index()
     {
         
-       $hasil=$this->beasiswa_model->pembagi();
-       //insert data ke db temp
-       for ($i=0; $i < count($hasil); $i++) { 
-           $this->beasiswa_model->insert_data($hasil[$i],'tmp_pembagi');
-       }
-       die();
+    //    $hasil=$this->beasiswa_model->pembagi();
+    //    //insert data ke db temp
+    //    for ($i=0; $i < count($hasil); $i++) { 
+    //        $this->beasiswa_model->insert_data($hasil[$i],'tmp_pembagi');
+    //    }
+    //    die();
 
   
-       $ternormalisasi=$this->beasiswa_model->Matrik_ternormalisasi();
-        echo "<pre>"; print_r($ternormalisasi); die();
+    //    $ternormalisasi=$this->beasiswa_model->Matrik_ternormalisasi();
+    //     echo "<pre>"; print_r($ternormalisasi); die();
 
-          for ($i=0; $i < count($ternormalisasi); $i++) { 
-              $this->beasiswa_model->insert_data($ternormalisasi[$i],'tmp_ternormalisasi');
-          }
-          die();
+    //       for ($i=0; $i < count($ternormalisasi); $i++) { 
+    //           $this->beasiswa_model->insert_data($ternormalisasi[$i],'tmp_ternormalisasi');
+    //       }
+    //       die();
 
+
+    //mulai mencoba 1
+    $sql = 'SELECT * FROM kriteria';
+    $result=$this->db->query($sql)->result_array();
+    //-- menyiapkan variable penampung berupa array
+    $kriteria=array();
+    $bobot=array();
+    //-- melakukan iterasi pengisian array untuk tiap record data yang didapat
+    foreach ($result as $row) {
+        $kriteria[$row['id_kriteria']]=array($row['nama_kriteria'],$row['keterangan']);
+        $bobot[$row['id_kriteria']]=$row['keterangan'];
+    }
+
+    //langkah ke 2
+    $sql = 'SELECT * FROM mahasiswa';
+    $result=$this->db->query($sql)->result_array();
+    //-- menyiapkan variable penampung berupa array
+    $mahasiswa=array();
+    //-- melakukan iterasi pengisian array untuk tiap record data yang didapat
+    foreach ($result as $row) {
+        $mahasiswa[$row['id_mahasiswa']]=$row['nama_mahasiswa'];
+    }
+
+    //langkah ke 3 
+    //-- query untuk mendapatkan semua data sample penilaian di tabel top_sample
+    $sql = 'SELECT * FROM nilai ORDER BY id_mahasiswa,id_kriteria';
+    $result=$this->db->query($sql)->result_array();
+    // $result = $this->db->query($sql);
+    //-- menyiapkan variable penampung berupa array
+    $sample=array();
+    //-- melakukan iterasi pengisian array untuk tiap record data yang didapat
+    foreach ($result as $row) {
+        //-- jika array $sample[$row['id_alternatif']] belum ada maka buat baru
+        //-- $row['id_alternatif'] adalah id kandidat/alternatif
+        if (!isset($sample[$row['id_mahasiswa']])) {
+            $sample[$row['id_mahasiswa']] = array();
+        }
+        $sample[$row['id_mahasiswa']][$row['id_kriteria']] = $row['nilai'];
+    }
+    //-- menjadikan data $sample sebagai matrik keputusan $X
+    //langkah ke 4
+    $X=$sample;
+    //-- melakukan iterasi utk setiap alternatif
+    foreach ($X as $id_mahasiswa=>$a_kriteria) {
+        echo "<tr>";
+        //-- melakukan iterasi utk setiap kriteria
+        foreach($a_kriteria as $id_kriteria=>$nilai){
+            echo "<td>{$nilai}</td><br>";
+        }
+        echo "</tr>";
+    }
+
+    //langkah ke 5
+    
+    //-- inisialisasi array pembagi
+    $pembagi=array();
+    //-- melakukan iterasi utk setiap kriteria
+    foreach($kriteria as $id_kriteria=>$value){
+        $pembagi[$id_kriteria]=0;
+        //-- melakukan iterasi utk setiap alternatif
+        foreach($mahasiswa as $id_mahasiswa=>$a_value){
+            $pembagi[$id_kriteria]=pow($X[$id_mahasiswa][$id_kriteria],2);
+        }
+    }
+    //-- inisialisasi matrik Normalisasi R
+    $R=array();
+    //-- melakukan iterasi utk setiap masiswa
+    foreach($X as $id_mahasiswa=>$a_kriteria) {
+        $R[$id_mahasiswa]=array();
+        //-- melakukan iterasi utk setiap kriteria
+        foreach($a_kriteria as $id_kriteria=>$nilai){
+            $R[$id_mahasiswa][$id_kriteria]=$nilai/sqrt($pembagi[$id_kriteria]);
+        }
+    }        
         $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
-        $this->load->view('hitung/data_hasil',$data);
+        $this->load->view('hitung/data_hasil');
         $this->load->view('templates/footer');
  //json_endcode($pembagi);
     // }
--- application/views/hitung/data_hasil.php
+++ application/views/hitung/data_hasil.php
@@ -3,9 +3,10 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Perhitungan </h1>
-        
+       
+                    
                     <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
+                    <!-- <div class="card shadow mb-4">
                         <div class="card-header py-3">
                         </div>
                         <?php echo $this->session->flashdata('pesan') ?>
@@ -61,7 +62,7 @@
                                 </table>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
 
                 </div>
                 <!-- /.container-fluid -->
